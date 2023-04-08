01fbff800b038805fb7f6d8f76482e7b9ee02232
Noor Aldeen <anooraldeen9@gmail.com>
2023-04-08 11:19:36 +0200

    create component folder

---
+++ src/Components/CatProduct/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import CatProductLeft from '../CatProductLeft'
+
+const CatProduct = () => {
+  return (
+    <div>
+        <CatProductLeft/>
+    </div>
+  )
+}
+
+export default CatProduct
---
+++ src/Components/CatProductLeft/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CatProductLeft = () => {
+  return (
+    <div>CatProductLeft</div>
+  )
+}
+
+export default CatProductLeft
