01d3a36d679011823c9aaa114f87f7b3a820985e
Noor Aldeen <anooraldeen9@gmail.com>
2023-04-11 20:29:49 +0200

    finish navbar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import * as React from 'react';
 import CatProduct from './Components/CatProduct'
+import Header from './Components/Header';
+import Navigation from './Components/Navigation'
 export default function BasicButtons() {
   return (
+    <div>
+    <Header/>
+    <Navigation/>
     <CatProduct/>
+    </div>
   );
 }
